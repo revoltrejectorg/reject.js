dfd5d85fa29135629c319c9a69bc6835f810874e
V3L0C1T13S <turtletester20@gmail.com>
2022-06-03 10:00:36 -0400

    feat: guild manager cache implementation

--- src/DiscordAPI_Stubs/Managers/GuildManager.ts
+++ src/DiscordAPI_Stubs/Managers/GuildManager.ts
@@ -5,5 +5,9 @@ import { Guild } from "../Guild";
 export class GuildManager extends CachedManager {
   constructor(client: RevoltClient, iterable = false) {
     super(client, Guild, iterable);
+    // FIXME: May cause performance issues with uncached guilds.
+    this.revoltClient.servers.forEach((server, key) => {
+      this._add(server, true, { id: key });
+    });
   }
 }
