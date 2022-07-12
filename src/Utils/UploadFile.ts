de220d4f9665fce6dd5ad797f206a44f4b0757de
V3L0C1T13S <turtletester20@gmail.com>
2022-07-12 15:30:35 -0400

    feat: make UploadFile simpler

--- src/Utils/UploadFile.ts
+++ src/Utils/UploadFile.ts
@@ -1,42 +1,24 @@
 import axios from "axios";
+import FormData from "form-data";
 
 type AttachmentTag = "attachments";
 
-const boundary = "------REJECTJS";
-function appendFormData(name: string, data: any, fileName: string) {
-  const fileBuffer: Buffer[] = [];
-
-  if (!data) return;
-  let str = `\r\n--${boundary}\r\nContent-Disposition: form-data; name="${name}"; filename="${fileName}"`;
-  if (data instanceof Buffer) {
-    str += "\r\nContent-Type: application/octet-stream";
-  } else if (data instanceof Object) {
-    str += "\r\nContent-Type: application/json";
-    // eslint-disable-next-line no-param-reassign
-    data = Buffer.from(JSON.stringify(data));
-  } else {
-    // eslint-disable-next-line no-param-reassign
-    data = Buffer.from(`${data}`);
-  }
-
-  fileBuffer.push(Buffer.from(`${str}\r\n\r\n`));
-  fileBuffer.push(data);
-
-  fileBuffer.push(Buffer.from(`\r\n--${boundary}--`));
-  return fileBuffer;
+export type revoltAttachmentResponse = {
+  id: string;
 }
 
 export async function UploadFile(
   file: { name: string; file: Buffer },
   type: AttachmentTag = "attachments",
 ) {
-  const data = Buffer.concat(appendFormData("file", file.file, file.name)!);
+  const data = new FormData();
+  data.append("file", file.file, { filename: file.name });
 
-  const response = (await (
-    await axios.post(`https://autumn.revolt.chat/${type}`, data, {
-      headers: { "Content-Type": `multipart/form-data; boundary=${boundary}` },
-    })
-  ).data) as unknown as { id: string };
+  const response = (await axios.post(
+    `https://autumn.revolt.chat/${type}`,
+    data,
+    { headers: data.getHeaders() },
+  )).data as unknown as revoltAttachmentResponse;
 
   return response.id;
 }
